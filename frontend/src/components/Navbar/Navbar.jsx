409c34900f452883edc6900db33c92c466f1a367
Jakub Putowski <jakub.putowski@microsoft.wsei.edu.pl>
2025-03-23 18:13:51 +0100

    dodanie logo i ikony

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,39 +5,43 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false); // Stan do kontrolowania widoczności menu
 
   return (
-    <nav className="fixed top-0 left-0 right-0 w-full p-4 shadow-lg z-10 border-b bg-[#151924] text-[#ede9fe]">
-      {/* Hamburger menu dla urządzeń mobilnych */}
-      <div className="flex justify-between items-center">
-        <h1 className="text-lg font-bold">
-          <img src="./favicon.ico" alt="Ikona"></img>
-        </h1>
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="md:hidden text-[#ede9fe] focus:outline-none"
-        >
-          {/* Ikona hamburgera */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 5.25h16.5m-16.5 6h16.5m-16.5 6h16.5"
-            />
-          </svg>
-        </button>
+    <nav className="fixed top-0 left-0 right-0 w-full h-16 p-4 shadow-lg z-10 border-b bg-[#151924] text-[#ede9fe] flex items-center justify-between">
+      {/* Logo */}
+      <div className="flex items-center h-full">
+        <img
+          src="./images/logo.jpg"
+          alt="Logo"
+          className="h-full w-auto" // H: pełna wysokość navbaru, W: proporcjonalna szerokość
+        />
       </div>
+
+      {/* Hamburger menu dla urządzeń mobilnych */}
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="md:hidden text-[#ede9fe] focus:outline-none"
+      >
+        {/* Ikona hamburgera */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 5.25h16.5m-16.5 6h16.5m-16.5 6h16.5"
+          />
+        </svg>
+      </button>
 
       {/* Menu nawigacyjne */}
       <ul
         className={`${
-          isOpen ? "block" : "hidden"
-        } md:flex justify-center space-x-4 mt-4 md:mt-0 md:space-x-8`}
+          isOpen ? "flex" : "hidden"
+        } md:flex flex-col md:flex-row justify-center items-center absolute md:static top-16 left-0 w-full md:w-auto bg-[#151924] md:bg-transparent space-y-4 md:space-y-0 md:space-x-8`}
       >
         <li>
           <a
